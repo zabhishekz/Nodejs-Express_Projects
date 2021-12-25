d4f60ae011a2f2e947895ada6678f2ea1639be53
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-25 14:35:19 +0530

    testing all routes and controllers

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -12,10 +12,5 @@ app.get('/hello',(req,res)=>{
 })
 
 app.use('/api/v1/tasks',tasks)
-//app.get('/api/v1/tasks');         -- get all the tasks
-//app.post('/api/v1/tasks');        -- create a new task
-//app.get('/api/v1/tasks/:id');     -- get single task
-//app.patch('/api/v1/tasks/:id');   -- update task
-//app.delete('/api/v1/tasks/:id');  -- delete task 
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
--- 01-task-manager/controllers/tasks.js
+++ 01-task-manager/controllers/tasks.js
@@ -1,5 +1,17 @@
 const getAllTasks = (req,res)=>{
-    res.send("all items from the file");
+    res.send("get all tasks");
+}
+const createTask= (req,res)=>{
+    res.json(req.body);
+}
+const getTask= (req,res)=>{
+    res.json({id:req.params.id});
+}
+const updateTask= (req,res)=>{
+    res.send("update task");
+}
+const deleteTask= (req,res)=>{
+    res.send("delete task");
 }
 
-module.exports = { getAllTasks };
+module.exports = { getAllTasks, createTask,getTask, updateTask, deleteTask };
