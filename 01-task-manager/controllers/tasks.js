e093bc5da96e732966dbaea13bd5ae27f1aeccb2
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-25 17:56:43 +0530

    Task Schema

--- 01-task-manager/controllers/tasks.js
+++ 01-task-manager/controllers/tasks.js
@@ -1,15 +1,22 @@
+const Task = require('../models/Task')
+
 const getAllTasks = (req,res)=>{
     res.send("get all tasks");
 }
-const createTask= (req,res)=>{
-    res.json(req.body);
+
+const createTask = async (req,res)=>{
+    const task = await Task.create(req.body)
+    res.status(201).json({ task });
 }
+
 const getTask= (req,res)=>{
     res.json({id:req.params.id});
 }
+
 const updateTask= (req,res)=>{
     res.send("update task");
 }
+
 const deleteTask= (req,res)=>{
     res.send("delete task");
 }
