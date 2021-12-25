deb57d6d7d9551d5e73cc486942373d8e2ad8d7c
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-25 22:13:19 +0530

    Middlwares- notfound and asyncWrapper

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -2,16 +2,17 @@ const express = require ('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
+const notFound = require('./middleware/not-found')
 
 require('dotenv').config();
+
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use(express.static('./public'));
 
-app.get('/hello',(req,res)=>{
-    res.send("Task Manager App");
-})
-
+//routes
 app.use('/api/v1/tasks',tasks)
+app.use(notFound);
 
 const port = 3000;
 
