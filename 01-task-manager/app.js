8203e3afe9000865feba90247fb3194de55495df
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-25 17:04:30 +0530

    Setting up connection string in .env

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -2,8 +2,8 @@ const express = require ('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-const port = 3000;
 
+require('dotenv').config();
 //middleware
 app.use(express.json())
 
@@ -13,9 +13,11 @@ app.get('/hello',(req,res)=>{
 
 app.use('/api/v1/tasks',tasks)
 
+const port = 3000;
+
 const start = async() =>{
     try{
-        await connectDB();
+        await connectDB(process.env.MONGO_URI);
         //here we only want the server to start if connect to DB is establihed , thus awe use async and await
         app.listen(port, console.log(`Server is listening on port ${port}...`));
     }
--- 01-task-manager/db/connect.js
+++ 01-task-manager/db/connect.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 
-const connectionString = 
-'mongodb+srv://abhishekV:Abhishek90@cluster0.tsxbz.mongodb.net/01-TASK_MANAGER?retryWrites=true&w=majority';
-
 const connectDB = (url) =>{
-    return mongoose.connect(connectionString,{
+    return mongoose.connect(url,{
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
