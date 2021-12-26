13abf6d4a6b43e947a22cdcbc3b635ed114bc089
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-26 18:36:58 +0530

    find functionlaity example 1

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -1,10 +1,12 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req,res) => {
-    throw new Error('testing async errors')
-    res.status(200).json({msg: 'prodcuts testing route'})
+    const products = await Product.find({featured: true})
+    res.status(200).json({products, nbHits: products.length})
 }  
 
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg: 'prodcuts testing route'})
+    res.status(200).json({msg: 'products testing route'})
 }  
 
 module.exports = {
