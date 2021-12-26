191e3d9d54f420623c79b403848757992666c409
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-26 19:09:29 +0530

    custom find functionality using query string

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -1,12 +1,15 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({name: 'vase table'})
+    const products = await Product.find({
+        name: 'vase table'
+    })
     res.status(200).json({products, nbHits: products.length})
 }  
 
 const getAllProducts = async (req,res) => {
-    res.status(200).json({msg: 'products testing route'})
+    const products = await Product.find(req.query);
+    res.status(200).json({products, nbHits: products.length})
 }  
 
 module.exports = {
