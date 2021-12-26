3c1207f3607bbbc387d95eef2bdb2b8a7130c27e
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-26 23:06:50 +0530

    Select certain fields

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({}).select('name price')
     res.status(200).json({products, nbHits: products.length})
 }  
 
 const getAllProducts = async (req,res) => {
-    const { featured, company, name, sort } = req.query  
+    const { featured, company, name, sort, fields } = req.query  
     const queryObject = {}
 
     if(featured){
@@ -26,6 +26,10 @@ const getAllProducts = async (req,res) => {
     }
     else{
         result = result.sort('createdAt')
+    }
+    if(fields){
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
     }
     const products = await result
     res.status(200).json({products, nbHits: products.length})
