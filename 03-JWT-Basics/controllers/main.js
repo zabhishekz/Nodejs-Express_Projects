3e6fa7b45ddec2bd22d14eb323874f638a1ea27c
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-27 19:43:15 +0530

    Token signing and recieving

--- 03-JWT-Basics/controllers/main.js
+++ 03-JWT-Basics/controllers/main.js
@@ -4,6 +4,7 @@
 
 //set up authentication so only the request with JWT can access the dashboard
 
+const jwt = require('jsonwebtoken')
 const CustomAPIError = require('../errors/custom-error')
 
 const login = async (req,res) => {
@@ -15,11 +16,27 @@ const login = async (req,res) => {
         throw new CustomAPIError('please provide email and password',400)
     }
 
+    //just for demo, generally provided by DB
+    const id = new Date().getDate()
+
+    //try to keep payload small
+    //just for demo, in production use long, complex, and unguessable string value !!!
+    const token = jwt.sign({id,username},process.env.JWT_SECRET,{expiresIn: '30d'})
+    res.status(200).json({msg: 'user created', token})
+
     console.log(username, password);
     res.send('Fake login/Register/Signup Route')
 }
 
 const dashboard = async (req,res) => {
+    const authHeader = req.headers.authorization
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        throw new CustomAPIError('No token provided',401)
+    }
+    const token = authHeader.split(' ')[1]
+    console.log(token);
+
+    console.log(req.headers)
     const luckyNumber = Math.floor(Math.random()*100)
     res.status(200).json({msg : `Hello, John Doe`, secret: `Here is your authorized data, your lucky number is ${luckyNumber}`})
 }
