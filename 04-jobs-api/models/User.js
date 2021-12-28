ee466ef492d9bc8a29bed1d813547dfca5e3f848
Abhishek Verma <zabhishek.verma@gmail.com>
2021-12-28 20:58:07 +0530

    Instance methods on a schema

--- 04-jobs-api/models/User.js
+++ 04-jobs-api/models/User.js
@@ -1,5 +1,6 @@
 const mongoose  = require('mongoose');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserSchema = mongoose.Schema({
     name: {
@@ -25,5 +26,9 @@ UserSchema.pre('save', async function(){
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password,salt)
 })
+
+UserSchema.methods.createJWT = function() {
+    return jwt.sign({userId:this._id, name:this.name}, 'jwtsecret', {expiresIn: '30d'})
+}
 
 module.exports = mongoose.model('User', UserSchema)
